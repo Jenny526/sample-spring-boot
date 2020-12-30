dad49db8ee4f92c258a36b328bfe49d0d4d75acf
guo jintao <guoj0019@e.ntu.edu.sg>
2020-12-30 19:04:59 +0800

    init

---
+++ src/main/java/com/tool/SampleRestApiApplication.java
@@ -0,0 +1,21 @@
+package com.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SampleRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestApiApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+
+}
